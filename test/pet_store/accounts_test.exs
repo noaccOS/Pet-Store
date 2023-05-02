3a65ac242cc52aa37b4d93e67f30024783b33d72
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-02 14:45:57 +0200

    accounts_test.exs: remove session_token references
    just as in the source code, replace the `*_session_token` functions with the `_token` functions.
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- test/pet_store/accounts_test.exs
+++ test/pet_store/accounts_test.exs
@@ -299,7 +299,7 @@ defmodule PetStore.AccountsTest do
     end
 
     test "deletes all tokens for the given user", %{user: user} do
-      _ = Accounts.generate_user_session_token(user)
+      _ = Accounts.generate_user_token(user)
 
       {:ok, _} =
         Accounts.update_user_password(user, valid_user_password(), %{
@@ -310,13 +310,13 @@ defmodule PetStore.AccountsTest do
     end
   end
 
-  describe "generate_user_session_token/1" do
+  describe "generate_user_token/1" do
     setup do
       %{user: user_fixture()}
     end
 
     test "generates a token", %{user: user} do
-      token = Accounts.generate_user_session_token(user)
+      token = Accounts.generate_user_token(user)
       assert user_token = Repo.get_by(UserToken, token: token)
       assert user_token.context == "session"
 
@@ -331,34 +331,34 @@ defmodule PetStore.AccountsTest do
     end
   end
 
-  describe "get_user_by_session_token/1" do
+  describe "get_user_by_token/1" do
     setup do
       user = user_fixture()
-      token = Accounts.generate_user_session_token(user)
+      token = Accounts.generate_user_token(user)
       %{user: user, token: token}
     end
 
     test "returns user by token", %{user: user, token: token} do
-      assert session_user = Accounts.get_user_by_session_token(token)
+      assert session_user = Accounts.get_user_by_token(token)
       assert session_user.id == user.id
     end
 
     test "does not return user for invalid token" do
-      refute Accounts.get_user_by_session_token("oops")
+      refute Accounts.get_user_by_token("oops")
     end
 
     test "does not return user for expired token", %{token: token} do
       {1, nil} = Repo.update_all(UserToken, set: [inserted_at: ~N[2020-01-01 00:00:00]])
-      refute Accounts.get_user_by_session_token(token)
+      refute Accounts.get_user_by_token(token)
     end
   end
 
-  describe "delete_user_session_token/1" do
+  describe "delete_user_token/1" do
     test "deletes the token" do
       user = user_fixture()
-      token = Accounts.generate_user_session_token(user)
-      assert Accounts.delete_user_session_token(token) == :ok
-      refute Accounts.get_user_by_session_token(token)
+      token = Accounts.generate_user_token(user)
+      assert Accounts.delete_user_token(token) == :ok
+      refute Accounts.get_user_by_token(token)
     end
   end
 
@@ -494,7 +494,7 @@ defmodule PetStore.AccountsTest do
     end
 
     test "deletes all tokens for the given user", %{user: user} do
-      _ = Accounts.generate_user_session_token(user)
+      _ = Accounts.generate_user_token(user)
       {:ok, _} = Accounts.reset_user_password(user, %{password: "new valid password"})
       refute Repo.get_by(UserToken, user_id: user.id)
     end
