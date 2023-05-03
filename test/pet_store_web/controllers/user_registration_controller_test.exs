6d210b6489d2b1e830145c40d25cc851769a7551
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-03 17:42:59 +0200

    user_registration_contreller_test.exs: remove inapplicable tests
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- test/pet_store_web/controllers/user_registration_controller_test.exs
+++ test/pet_store_web/controllers/user_registration_controller_test.exs
@@ -3,22 +3,6 @@ defmodule PetStoreWeb.UserRegistrationControllerTest do
 
   import PetStore.AccountsFixtures
 
-  describe "GET /users/register" do
-    test "renders registration page", %{conn: conn} do
-      conn = get(conn, ~p"/users/register")
-      response = html_response(conn, 200)
-      assert response =~ "Register"
-      assert response =~ ~p"/users/log_in"
-      assert response =~ ~p"/users/register"
-    end
-
-    test "redirects if already logged in", %{conn: conn} do
-      conn = conn |> log_in_user(user_fixture()) |> get(~p"/users/register")
-
-      assert redirected_to(conn) == ~p"/"
-    end
-  end
-
   describe "POST /users/register" do
     @tag :capture_log
     test "creates account and logs the user in", %{conn: conn} do
