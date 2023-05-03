e13b04f3cac9542ba6d99a097c0d3c1f0cbfeb7c
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-03 18:31:10 +0200

    user_reset_password_controller_test.exs: fix tests for project
    use json responses
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- test/pet_store_web/controllers/user_reset_password_controller_test.exs
+++ test/pet_store_web/controllers/user_reset_password_controller_test.exs
@@ -17,10 +17,9 @@ defmodule PetStoreWeb.UserResetPasswordControllerTest do
           "user" => %{"email" => user.email}
         })
 
-      assert redirected_to(conn) == ~p"/"
-
-      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~
-               "If your email is in our system"
+      response = json_response(conn, 200)
+      assert response["status"] == "ok"
+      assert response["message"] =~ "If your email is in our system"
 
       assert Repo.get_by!(Accounts.UserToken, user_id: user.id).context == "reset_password"
     end
@@ -31,10 +30,9 @@ defmodule PetStoreWeb.UserResetPasswordControllerTest do
           "user" => %{"email" => "unknown@example.com"}
         })
 
-      assert redirected_to(conn) == ~p"/"
-
-      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~
-               "If your email is in our system"
+      response = json_response(conn, 200)
+      assert response["status"] == "ok"
+      assert response["message"] =~ "If your email is in our system"
 
       assert Repo.all(Accounts.UserToken) == []
     end
@@ -59,13 +57,8 @@ defmodule PetStoreWeb.UserResetPasswordControllerTest do
           }
         })
 
-      assert redirected_to(conn) == ~p"/users/log_in"
-      refute get_session(conn, :user_token)
-
-      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~
-               "Password reset successfully"
-
-      assert Accounts.get_user_by_email_and_password(user.email, "new valid password")
+      assert {:ok, _} =
+               Accounts.fetch_user_by_email_and_password(user.email, "new valid password")
     end
 
     test "does not reset password on invalid data", %{conn: conn, token: token} do
@@ -77,15 +70,17 @@ defmodule PetStoreWeb.UserResetPasswordControllerTest do
           }
         })
 
-      assert html_response(conn, 200) =~ "something went wrong"
+      response = json_response(conn, 200)
+      assert response["status"] == "error"
+      assert response["message"] == "Error encountered during password reset."
     end
 
     test "does not reset password with invalid token", %{conn: conn} do
       conn = put(conn, ~p"/users/reset_password/oops")
-      assert redirected_to(conn) == ~p"/"
 
-      assert Phoenix.Flash.get(conn.assigns.flash, :error) =~
-               "Reset password link is invalid or it has expired"
+      response = json_response(conn, 200)
+      assert response["status"] == "error"
+      assert response["message"] == "Reset password link is invalid or it has expired."
     end
   end
 end
