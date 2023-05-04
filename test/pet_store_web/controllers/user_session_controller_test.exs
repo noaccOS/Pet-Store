ec24735983b0b4b34f5ee2186ead48fa466dee4a
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-04 11:25:03 +0200

    user_session_controller_test.exs: fix applicable tests
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- test/pet_store_web/controllers/user_session_controller_test.exs
+++ test/pet_store_web/controllers/user_session_controller_test.exs
@@ -14,15 +14,18 @@ defmodule PetStoreWeb.UserSessionControllerTest do
           "user" => %{"email" => user.email, "password" => valid_user_password()}
         })
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == ~p"/"
+      token = conn.assigns[:user_token]
+      assert token
 
-      # Now do a logged in request and assert on the menu
-      conn = get(conn, ~p"/")
-      response = html_response(conn, 200)
-      assert response =~ user.email
-      assert response =~ ~p"/users/settings"
-      assert response =~ ~p"/users/log_out"
+      # Now do a logged in request and assert the outcome
+      conn =
+        build_conn()
+        |> Plug.Conn.put_req_header("authorization", "Bearer #{token}")
+        |> delete(~p"/users/log_out")
+
+      response = json_response(conn, 200)
+      assert response["status"] == "ok"
+      assert response["message"] == "User logged out successfully."
     end
 
     test "emits error message with invalid credentials", %{conn: conn, user: user} do
@@ -31,18 +34,20 @@ defmodule PetStoreWeb.UserSessionControllerTest do
           "user" => %{"email" => user.email, "password" => "invalid_password"}
         })
 
-      response = html_response(conn, 200)
-      assert response =~ "Log in"
-      assert response =~ "Invalid email or password"
+      response = json_response(conn, 200)
+      assert response["status"] == "error"
+      assert response["message"] == "Invalid email or password"
     end
   end
 
   describe "DELETE /users/log_out" do
     test "logs the user out", %{conn: conn, user: user} do
       conn = conn |> log_in_user(user) |> delete(~p"/users/log_out")
-      assert redirected_to(conn) == ~p"/"
-      refute get_session(conn, :user_token)
-      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "Logged out successfully"
+      refute conn.assigns[:user_token]
+
+      response = json_response(conn, 200)
+      assert response["status"] == "ok"
+      assert response["message"] == "User logged out successfully."
     end
 
     test "succeeds even if the user is not logged in", %{conn: conn} do
