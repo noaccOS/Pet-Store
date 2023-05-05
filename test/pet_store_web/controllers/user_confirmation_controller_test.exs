71cba99b76600f5a31f013b7ac68c0a4f988b03f
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-05 16:19:23 +0200

    fallback controller init
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- test/pet_store_web/controllers/user_confirmation_controller_test.exs
+++ test/pet_store_web/controllers/user_confirmation_controller_test.exs
@@ -1,83 +1,46 @@
-defmodule PetStoreWeb.UserConfirmationControllerTest do
-  use PetStoreWeb.ConnCase, async: true
+defmodule PetStoreWeb.UserConfirmationController do
+  use PetStoreWeb, :controller
 
   alias PetStore.Accounts
-  alias PetStore.Repo
-  import PetStore.AccountsFixtures
-  require IEx
 
-  setup do
-    %{user: user_fixture()}
+  def create(conn, %{"user" => %{"email" => email}}) do
+    with {:ok, user} <- Accounts.fetch_user_by_email(email) do
+      Accounts.deliver_user_confirmation_instructions(
+        user,
+        &"POST #{url(~p[/users/confirm/#{&1}])}"
+      )
+    end
+
+    render(
+      conn,
+      :message_ok,
+      msg:
+        "If your email is in our system and it has not been confirmed yet, " <>
+          "you will receive an email with instructions shortly."
+    )
   end
 
-  describe "POST /users/confirm" do
-    @tag :capture_log
-    test "sends a new confirmation token", %{conn: conn, user: user} do
-      post(conn, ~p"/users/confirm", %{
-        "user" => %{"email" => user.email}
-      })
+  # Do not log in the user after confirmation to avoid a
+  # leaked token giving the user access to the account.
+  def update(conn, %{"token" => token}) do
+    case Accounts.confirm_user(token) do
+      {:ok, _} ->
+        render(conn, :message_ok, msg: "User confirmed successfully.")
 
-      assert Repo.get_by!(Accounts.UserToken, user_id: user.id).context == "confirm"
-    end
+      :error ->
+        # If there is a current user and the account was already confirmed,
+        # then odds are that the confirmation link was already visited, either
+        # by some automation or by the user themselves, so we redirect without
+        # a warning message.
+        case conn.assigns do
+          %{current_user: %{confirmed_at: confirmed_at}} when not is_nil(confirmed_at) ->
+            render(conn, :message_ok, msg: nil)
 
-    test "does not send confirmation token if User is confirmed", %{conn: conn, user: user} do
-      Repo.update!(Accounts.User.confirm_changeset(user))
-
-      post(conn, ~p"/users/confirm", %{
-        "user" => %{"email" => user.email}
-      })
-
-      refute Repo.get_by(Accounts.UserToken, user_id: user.id)
-    end
-
-    test "does not send confirmation token if email is invalid", %{conn: conn} do
-      post(conn, ~p"/users/confirm", %{
-        "user" => %{"email" => "unknown@example.com"}
-      })
-
-      assert Repo.all(Accounts.UserToken) == []
-    end
-  end
-
-  describe "POST /users/confirm/:token" do
-    test "confirms the given token once", %{conn: conn, user: user} do
-      token =
-        extract_user_token(fn url ->
-          Accounts.deliver_user_confirmation_instructions(user, url)
-        end)
-
-      conn = post(conn, ~p"/users/confirm/#{token}")
-
-      user = Accounts.fetch_user!(user.id)
-
-      assert user.confirmed_at
-      refute conn.assigns[:user_token]
-      assert Repo.all(Accounts.UserToken) == []
-
-      # When not logged in
-      conn = post(conn, ~p"/users/confirm/#{token}")
-      resp = json_response(conn, 200)
-      assert resp["status"] == "error"
-      assert resp["message"] == "User confirmation link is invalid or it has expired."
-
-      # When logged in
-      conn =
-        build_conn()
-        |> log_in_user(user)
-        |> post(~p"/users/confirm/#{token}")
-
-      resp = json_response(conn, 200)
-      assert resp["status"] == "ok"
-      refute resp["message"] == "User confirmation link is invalid or it has expired."
-    end
-
-    test "does not confirm email with invalid token", %{conn: conn, user: user} do
-      conn = post(conn, ~p"/users/confirm/oops")
-      resp = json_response(conn, 200)
-
-      assert resp["status"] == "error"
-      assert resp["message"] == "User confirmation link is invalid or it has expired."
-      refute Accounts.fetch_user!(user.id).confirmed_at
+          %{} ->
+            render(conn, :message_error,
+              msg: "User confirmation link is invalid or it has expired."
+            )
+        end
     end
   end
 end
