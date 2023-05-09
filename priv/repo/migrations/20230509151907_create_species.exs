43a58462752e663a694ac80cc681052bf26becfb
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-09 17:48:13 +0200

    feat(migration)!: use name as species' primary key
    the name uniquely identifies a species so there is no need for another id.
    also creates an index for species for performance reasons.
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- priv/repo/migrations/20230509151907_create_species.exs
+++ priv/repo/migrations/20230509151907_create_species.exs
@@ -2,11 +2,13 @@ defmodule PetStore.Repo.Migrations.CreateSpecies do
   use Ecto.Migration
 
   def change do
-    create table(:species) do
-      add :name, :string
+    create table(:species, primary_key: false) do
+      add :name, :string, primary_key: true
       add :family, :string
 
       timestamps()
     end
+
+    create index(:species, [:family])
   end
 end
