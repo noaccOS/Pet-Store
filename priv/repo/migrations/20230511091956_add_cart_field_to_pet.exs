da0c5208791da185e09fe240e5a21dbde96d2dc7
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-11 11:38:19 +0200

    feat(pet): implement `cart` field in migration
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- priv/repo/migrations/20230511091956_add_cart_field_to_pet.exs
+++ priv/repo/migrations/20230511091956_add_cart_field_to_pet.exs
@@ -2,6 +2,8 @@ defmodule PetStore.Repo.Migrations.AddCartFieldToPet do
   use Ecto.Migration
 
   def change do
-
+    alter table(:pets) do
+      add :cart_id, references(:carts, on_delete: :nullify)
+    end
   end
 end
