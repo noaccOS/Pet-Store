9dcac5f51405c91f613f1f4eb99f7d2ab1e0ffba
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-11 16:32:29 +0200

    fix(cart): cannot have fk with table's name
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- priv/repo/migrations/20230511090419_create_carts.exs
+++ priv/repo/migrations/20230511090419_create_carts.exs
@@ -4,11 +4,11 @@ defmodule PetStore.Repo.Migrations.CreateCarts do
   def change do
     create table(:carts) do
       add :completed_on, :naive_datetime
-      add :user, references(:users, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
     end
 
-    create index(:carts, [:user])
+    create index(:carts, [:user_id])
   end
 end
