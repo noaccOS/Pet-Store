bc49ebc18828eb2c9aaeed9ee53e99e1b77e8589
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-12 12:42:08 +0200

    feat(repo): implement fetch functions
    the ! variants are included for consistency, although they are
    exactly the same as the get variant
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- lib/pet_store/repo.ex
+++ lib/pet_store/repo.ex
@@ -2,4 +2,26 @@ defmodule PetStore.Repo do
   use Ecto.Repo,
     otp_app: :pet_store,
     adapter: Ecto.Adapters.Postgres
+
+  def fetch(resource, id, opts \\ []) do
+    case get(resource, id, opts) do
+      nil -> {:error, :not_found}
+      x -> {:ok, x}
+    end
+  end
+
+  def fetch_by(resource, attrs, opts \\ []) do
+    case get_by(resource, attrs, opts) do
+      nil -> {:error, :not_found}
+      x -> {:ok, x}
+    end
+  end
+
+  def fetch!(resource, id, opts \\ []) do
+    get!(resource, id, opts)
+  end
+
+  def fetch_by!(resource, attrs, opts \\ []) do
+    get_by!(resource, attrs, opts)
+  end
 end
