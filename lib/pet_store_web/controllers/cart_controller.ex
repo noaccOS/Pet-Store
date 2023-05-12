9e95e56891468c2a39411c17a554aa8e9af52880
Francesco Noacco <francesco.noacco@secomind.com>
2023-05-12 11:44:17 +0200

    refactor(cart): remove unneeded methods from controller
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- lib/pet_store_web/controllers/cart_controller.ex
+++ lib/pet_store_web/controllers/cart_controller.ex
@@ -11,32 +11,8 @@ defmodule PetStoreWeb.CartController do
     render(conn, :index, carts: carts)
   end
 
-  def create(conn, %{"cart" => cart_params}) do
-    with {:ok, %Cart{} = cart} <- Shop.create_cart(cart_params) do
-      conn
-      |> put_status(:created)
-      |> render(:show, cart: cart)
-    end
-  end
-
   def show(conn, %{"id" => id}) do
     cart = Shop.get_cart!(id)
     render(conn, :show, cart: cart)
-  end
-
-  def update(conn, %{"id" => id, "cart" => cart_params}) do
-    cart = Shop.get_cart!(id)
-
-    with {:ok, %Cart{} = cart} <- Shop.update_cart(cart, cart_params) do
-      render(conn, :show, cart: cart)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    cart = Shop.get_cart!(id)
-
-    with {:ok, %Cart{}} <- Shop.delete_cart(cart) do
-      send_resp(conn, :no_content, "")
-    end
   end
 end
